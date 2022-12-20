933d9ef60e19b33ec9a07bd8c79c4a7ac6c7ff93
Christian Manzaraz <magnusmanz@gmail.com>
2022-12-20 15:09:26 -0300

    Initial Commit

---
+++ SystemViewControllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SystemViewControllers
+//
+//  Created by Christian Manzaraz on 20/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
